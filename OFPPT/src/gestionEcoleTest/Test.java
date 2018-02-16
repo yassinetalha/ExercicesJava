cc09f71929ec6103b9333aeac4a842769658d3d9
talhay <talhayassine90@gmail.com>
2018-02-16 11:10:38 +0100

    Dans la class Test nous avons créé des deux tableaux d'objet: Professeur et Spécialite. Cette classe nous permet d'afficher le résulatt.

--- OFPPT/src/gestionEcoleTest/Test.java
+++ OFPPT/src/gestionEcoleTest/Test.java
@@ -7,20 +7,38 @@ public class Test {
 
 	public static void main(String[] args) {
 		
-		// Creation des professeurs
-		Professeur prof1=new Professeur("SAFI","Amal","0622232425","safi.amal@gamail.com");
-		Professeur prof2=new Professeur("ALAMI","Said","0622232425","alami.said@gamail.com");
-		Professeur prof3=new Professeur("ALAOUI","Reda","0622232425","alaoui.reda@gamail.com");
-		Professeur prof4=new Professeur("KAMALI","Imane","0622232425","kamali.imane@gamail.com");
-		
 		// Creation des spécialités
+				Spécialite[] specialites = new Spécialite[5];
+				specialites [0]= new Spécialite("S1", "JAVA/JEE");
+				specialites [1]= new Spécialite("S2", ".Net");
+				specialites [2]= new Spécialite("S3", "Gestion de Projet");
+				specialites [3]= new Spécialite("S4", "CISCO");
+				specialites [4]= new Spécialite("S5", "PHP");
+				
+		Professeur[] professeurs = new Professeur[4];
+		// Creation des professeurs
+		professeurs [0]=new Professeur("SAFI","Amal","0622232425","safi.amal@gamail.com",specialites [0]);
+		professeurs [1]=new Professeur("ALAMI","Said","0622232425","alami.said@gamail.com",specialites [0]);
+		professeurs [2]=new Professeur("ALAOUI","Reda","0622232425","alaoui.reda@gamail.com",specialites [3]);
+		professeurs [3]=new Professeur("KAMALI","Imane","0622232425","kamali.imane@gamail.com",specialites [3]);
 		
-		Spécialite s1= new Spécialite("000", "JAVA/JEE");
-		Spécialite s2= new Spécialite("001", ".Net");
-		Spécialite s3= new Spécialite("010", "Gestion de Projet");
-		Spécialite s4= new Spécialite("011", "CISCO");
-		Spécialite s5= new Spécialite("101", "PHP");
 		
+		
+		System.out.println("Professeur par spécialité :");
+		
+		for(Spécialite s: specialites) {
+			System.out.println("\t"+ s);
+			boolean flag=false;
+			for(Professeur p: professeurs) {
+				if(p.getSpecialite().getId()==s.getId()) {
+					System.out.println("\t\t"+p);
+					flag=true;
+				}
+			
+			}
+			
+			if(flag==false) System.out.println("\t\tAucun professeur dans cette spécialité");
+		}
 
 	}
 
